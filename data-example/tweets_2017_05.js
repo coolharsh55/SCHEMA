aaa01d1e7f8935bb9256beedf04c8ecc41d71fd9
Harshvardhan Pandit <me@harshp.com>
2020-07-29 20:39:51 +0100

    added schema sketches, facebook schema, sample data

---
+++ data-example/tweets_2017_05.js
@@ -0,0 +1,123 @@
+Grailbird.data.tweets_2017_05 = 
+[ {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ {
+      "name" : "EFF",
+      "screen_name" : "EFF",
+      "indices" : [ 10, 14 ],
+      "id_str" : "4816",
+      "id" : 4816
+    } ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 74, 97 ],
+      "url" : "https:\/\/t.co\/fDsLMNlna7",
+      "expanded_url" : "https:\/\/eff.org\/donate",
+      "display_url" : "eff.org\/donate"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "863795316167979008",
+  "text" : "I support @EFF because well, the recent news DOES say it all, doesn't it? https:\/\/t.co\/fDsLMNlna7",
+  "id" : 863795316167979008,
+  "created_at" : "2017-05-14 16:37:06 +0000",
+  "user" : {
+    "name" : "Harshvardhan Pandit",
+    "screen_name" : "coolharsh55",
+    "protected" : false,
+    "id_str" : "54597193",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/474956167509053440\/mUr2Q3I9_normal.jpeg",
+    "id" : 54597193,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ {
+      "name" : "Sarven Capadisli",
+      "screen_name" : "csarven",
+      "indices" : [ 3, 11 ],
+      "id_str" : "14188834",
+      "id" : 14188834
+    }, {
+      "name" : "W3C",
+      "screen_name" : "w3c",
+      "indices" : [ 44, 48 ],
+      "id_str" : "35761106",
+      "id" : 35761106
+    }, {
+      "name" : "W3C Social Web WG",
+      "screen_name" : "SocialWebWG",
+      "indices" : [ 66, 78 ],
+      "id_str" : "2413390255",
+      "id" : 2413390255
+    } ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ {
+      "indices" : [ 15, 38 ],
+      "url" : "https:\/\/t.co\/fzhFcxvO8e",
+      "expanded_url" : "https:\/\/www.w3.org\/TR\/ldn\/",
+      "display_url" : "w3.org\/TR\/ldn\/"
+    } ]
+  },
+  "geo" : { },
+  "id_str" : "859480560749727744",
+  "text" : "RT @csarven: \uD83D\uDE80\u00A0https:\/\/t.co\/fzhFcxvO8e is a @W3C Recommendation w @SocialWebWG  Let's continue to build an exciting ecosystem of #LinkedDat\u2026",
+  "retweeted_status" : {
+    "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+    "entities" : {
+      "user_mentions" : [ {
+        "name" : "W3C",
+        "screen_name" : "w3c",
+        "indices" : [ 31, 35 ],
+        "id_str" : "35761106",
+        "id" : 35761106
+      }, {
+        "name" : "W3C Social Web WG",
+        "screen_name" : "SocialWebWG",
+        "indices" : [ 53, 65 ],
+        "id_str" : "2413390255",
+        "id" : 2413390255
+      } ],
+      "media" : [ ],
+      "hashtags" : [ {
+        "text" : "LinkedData",
+        "indices" : [ 116, 127 ]
+      } ],
+      "urls" : [ {
+        "indices" : [ 2, 25 ],
+        "url" : "https:\/\/t.co\/fzhFcxvO8e",
+        "expanded_url" : "https:\/\/www.w3.org\/TR\/ldn\/",
+        "display_url" : "w3.org\/TR\/ldn\/"
+      } ]
+    },
+    "geo" : { },
+    "id_str" : "859375297069813764",
+    "text" : "\uD83D\uDE80\u00A0https:\/\/t.co\/fzhFcxvO8e is a @W3C Recommendation w @SocialWebWG  Let's continue to build an exciting ecosystem of #LinkedData applications",
+    "id" : 859375297069813764,
+    "created_at" : "2017-05-02 11:53:31 +0000",
+    "user" : {
+      "name" : "Sarven Capadisli",
+      "screen_name" : "csarven",
+      "protected" : false,
+      "id_str" : "14188834",
+      "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/640840970083299328\/lGRyMUeo_normal.jpg",
+      "id" : 14188834,
+      "verified" : false
+    }
+  },
+  "id" : 859480560749727744,
+  "created_at" : "2017-05-02 18:51:48 +0000",
+  "user" : {
+    "name" : "Harshvardhan Pandit",
+    "screen_name" : "coolharsh55",
+    "protected" : false,
+    "id_str" : "54597193",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/474956167509053440\/mUr2Q3I9_normal.jpeg",
+    "id" : 54597193,
+    "verified" : false
+  }
+} ]
